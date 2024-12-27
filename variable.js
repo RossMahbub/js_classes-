9c832bcb23cdfb87f4a1d157c54778767974ea61
RossMahbub <mahbubhasan90@gmail.com>
2024-12-28 05:15:04 +0600

    we know var

---
+++ variable.js
@@ -0,0 +1,18 @@
+// variable
+var boxOne;
+boxOne = "salt";
+console.log(boxOne , "This is line from 3");
+
+var boxTwo;
+boxTwo = "sugar";
+console.log(boxTwo, "This is line from 7");
+
+var boxThree;
+boxThree = "chilli";
+console.log(boxThree, "This is line from 11");
+
+var girlsName = "Jamila";
+console.log(girlsName, "15 line: before marriage");
+var girlsName = "Jamila yousuf";
+console.log(girlsName, "17 line: after marriage");
+
