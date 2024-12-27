8781a2c9d2d986dcbbc7c1b90eadd226da8448ff
RossMahbub <mahbubhasan90@gmail.com>
2024-12-28 05:32:14 +0600

    variable-let

---
+++ variable-let.js
@@ -0,0 +1,30 @@
+var husbandofJamilasNmae;
+husbandofJamilasNmae = "Yousuf";
+console.log(husbandofJamilasNmae);
+
+var husbandofJamilasNmae = "Yousuf";
+console.log(husbandofJamilasNmae);
+
+let husbandofJamilasNmae;
+husbandofJamilasNmae = "Yousuf";
+console.log(husbandofJamilasNmae);
+
+let husbandofJamilasNmae = "Yousuf";
+console.log(husbandofJamilasNmae);
+
+var husbandofJamilasNmae = "Yousuf";
+console.log(husbandofJamilasNmae);
+
+var husbandofJamilasNmae = "Yousuf Jamila";
+console.log(husbandofJamilasNmae);
+
+let husbandofJamilasNmae = "Yousuf";
+console.log(husbandofJamilasNmae);
+
+// let husbandofJamilasNmae = "Yousuf Jamila";
+// console.log(husbandofJamilasNmae);
+husbandofJamilasNmae = "Yousuf Yousuf";
+console.log(husbandofJamilasNmae);
+
+
+
