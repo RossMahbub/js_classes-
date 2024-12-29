31becd24e2bc6bfd93b6f9f3dd3d578d3374d419
RossMahbub <mahbubhasan90@gmail.com>
2024-12-29 20:24:59 +0600

    data

---
+++ data-types.js
@@ -0,0 +1,45 @@
+const heroName = "Allu Arjun";
+console.log(heroName);
+
+let name = "John";
+console.log(name);
+
+let greeting = "Hello";
+console.log(greeting);
+
+let message = ` ${greeting} welcome ${name}`;
+console.log(message);
+
+let age = 25;
+console.log(age);
+
+let temprature = 36.6;
+console.log(temprature);
+console.log(typeof temprature);
+
+let isEligibleForMarriage = true;
+console.log(isEligibleForMarriage);
+console.log(typeof isEligibleForMarriage);
+
+let isStudent = false;
+console.log(typeof isStudent);
+
+let yousufWifeName = null ;
+console.log(yousufWifeName);
+console.log(typeof yousufWifeName);
+
+let notAssigned;
+console.log(notAssigned);
+console.log(typeof notAssigned);
+
+let ross = {
+  fullName: "Ross Mahbub",
+  age: 20,
+  isStudent: true,
+  isMarried: false,
+  isJobHolder: false,
+};
+console.log(ross);
+console.log(ross.fullName);
+console.log(ross["isStudent"]);
+
