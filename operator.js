22461d3d51fbef0d12f65a738ce0ccf1368b4c89
RossMahbub <mahbubhasan90@gmail.com>
2024-12-31 18:15:02 +0600

    operator

--- operator.js
+++ operator.js
@@ -1,29 +1,35 @@
-let a =1;
-let b = 2;
-let total = a + b;
-console.log(total)
+// assition
+// let a =1;
+// let b = 2;
+// let total = a + b;
+// console.log(total);
 
-let a =1;
-let b = 2;
-let total = a - b;
-console.log(total)
+// subtraction
+// let a =1;
+// let b = 2;
+// let total = a - b;
+// console.log(total);
 
-let a =10;
-let b = 2;
-let total = a * b;
-console.log(total)
+// multiplication
+// let a =10;
+// let b = 2;
+// let total = a * b;
+// console.log(total);
 
-let a =10;
-let b = 2;
-let total = a / b;
-console.log(total)
+// // division
+// let a =10;
+// let b = 2;
+// let total = a / b;
+// console.log(total);
 
-let a =15;
-let b = 2;
-let total = a % b;
-console.log(total)
+// // modulas
+// let a =11;
+// let b = 2;
+// let total = a % b;
+// console.log(total);
 
+// wxponentiation
 let a =2;
 let b = 3;
 let total = a ** b;
-console.log(total)
+console.log(total);
