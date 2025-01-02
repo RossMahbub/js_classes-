f2bcd72ef421a854df61bf487d7430a1d6d8a2fd
RossMahbub <mahbubhasan90@gmail.com>
2025-01-02 21:27:41 +0600

    loop

---
+++ loop.js
@@ -0,0 +1,34 @@
+// console.log("Our Round is 1");
+// console.log("Our Round is 2");
+// console.log("Our Round is 3");
+// console.log("Our Round is 4");
+// console.log("Our Round is 5");
+// console.log("Our Round is 6");
+// console.log("Our Round is 7");
+// console.log("Our Round is 8");
+// console.log("Our Round is 9");
+// // console.log("Our Round is 10");
+
+// for(let round = 0; round < 10; round++){
+//   console.log(`our round is ${round}`);
+// }
+// console.log("loop is finished")
+
+// for(let i = 1; true <= 5; i ++){
+//   console.log(i)
+// }
+
+// let i = 1;
+// while(i<= 5){
+//   console.log(i);
+//     i++;
+  
+// }
+// console.log("while loop shesh")
+
+let i = 1;
+do{
+  console.log(i)
+  i++;
+}while(i <=5)
+  console.log("while loop shesh")
