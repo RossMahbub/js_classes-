0678f73d7890d4112123cfdaa9556dee921d883e
RossMahbub <mahbubhasan90@gmail.com>
2025-01-02 17:10:05 +0600

    function

--- function.js
+++ function.js
@@ -1,36 +1,157 @@
-let mohanVai = 500;
-let gift = 2000;
-const mohanVaiTotalTaka = mohanVai + gift;
-// console.log(mohanVaiTotalTaka);
+// let mohanVai = 500;
+// let gift = 2000;
+// const mohanVaiTotalTaka = mohanVai + gift;
+// // console.log(mohanVaiTotalTaka);
 
-let tufanVaiTaka = 1000;
-let tufanGift = 2000;
-const tufanViaiTotelTaka = tufanVaiTaka + tufanGift;
-// console.log(tufanViaiTotelTaka);
-
-
-
-// function totalOfSum(){
-//   let mohanBrothersTaka = 5000;
-//   let mohanBrothersGift = 2000;
-//   const totalTaka = mohanBrothersGift + mohanBrothersTaka;
-//   console.log(totalTaka);
-// // }
-// // totalOfSum()
-// // totalOfSum()
-// // totalOfSum()
-// // totalOfSum()
-// // totalOfSum()
-// // totalOfSum()
-// // totalOfSum()
+// let tufanVaiTaka = 1000;
+// let tufanGift = 2000;
+// const tufanViaiTotelTaka = tufanVaiTaka + tufanGift;
+// // console.log(tufanViaiTotelTaka);
 
 
 
-function sumOfSalary(salaryOfFirst, salaryOFSecond){
-  console.log(salaryOfFirst, salaryOFSecond)
-  // const salaryOfJanuary = 2000;
-  // const salaryOfFebruary = 4000;
-  // const totalSalary = salaryOfJanuary + salaryOfFebruary;
-  // console.log(totalSalary)
-}
-sumOfSalary(4000,4000)
+// // function totalOfSum(){
+// //   let mohanBrothersTaka = 5000;
+// //   let mohanBrothersGift = 2000;
+// //   const totalTaka = mohanBrothersGift + mohanBrothersTaka;
+// //   console.log(totalTaka);
+// // // }
+// // // totalOfSum()
+// // // totalOfSum()
+// // // totalOfSum()
+// // // totalOfSum()
+// // // totalOfSum()
+// // // totalOfSum()
+// // // totalOfSum()
+
+
+
+// function sumOfSalary(salaryOfFirst, salaryOFSecond){
+//   console.log(salaryOfFirst, salaryOFSecond)
+//   // const salaryOfJanuary = 2000;
+//   // const salaryOfFebruary = 4000;
+//   // const totalSalary = salaryOfJanuary + salaryOfFebruary;
+//   // console.log(totalSalary)
+// }
+// sumOfSalary(4000,4000)
+
+// function greet(){
+//   console.log("hello world")
+// }
+// greet();
+
+// function greet(name = 2024){
+//   console.log("hello, "  + name + "!")
+// }
+// greet("2025");
+// const newYear = ("2025");
+// greet();
+
+
+// function fullName(firstName, lastName){
+//   console.log(firstName, lastName)
+// };
+// fullName("Mahbub", "hasan");
+
+// function add(firstNumber, secondNumber){
+//   // console.log(firstNumber, secondNumber);
+//   const sum = firstNumber + secondNumber;
+//   return sum;
+// }
+// const a = 10;
+// const b = 20;
+
+// add(a, b)
+// const result =  add(a, b)
+// // console.log(result)
+
+// const totelTaka = result + 50;
+// console.log(totelTaka)
+
+
+// function sum(a, b){
+//   const totel = a + b;
+//   return totel;
+// }
+
+// const money = sum(10,10);
+// console.log(money);
+
+// const sum = (a,b) => {
+//   const totel = a + b;
+//   return totel;
+// }
+
+// const sum = (a,b) => a + b;
+// const money = sum(10,10);
+// console.log(money);
+
+// const createPerson = (name, age) =>{
+//   // console.log(name, age)
+//   const person = {
+//     fullName: name,
+//     hisAge: age
+//   };
+//   // console.log(person);
+//   return person;
+// }
+// // createPerson("Ross Mahbub", 30)
+// const rossMahbub = createPerson("Ross Mahbub", 30)
+// console.log(name, age)
+
+
+// const sayHello = function (a, b){
+//   console.log("hello world", a , b)
+// }
+// sayHello("a", "b")
+
+
+// const sayHello =  (a, b) => {
+//   console.log("hello world", a , b)
+// }
+// sayHello("a", "b")
+
+// (function(ab, cd){
+//   console.log("IIFE function", ab, cd)
+// })("ab", "cd")
+
+// ((ab, cd)=>{
+//   console.log("IIFE function", ab, cd)
+// })("ab", "cd")
+
+// function greet(name){
+//   console.log("Hello, " + name + "!")
+// }
+
+// function executeCallback(callback){
+//   callback("Alice")
+// }
+
+// executeCallback(greet);
+
+
+
+// function greet(name, callback){
+//   console.log("Hello" + name)
+//   callback()
+// }
+
+// function sayGoodbye(){
+//   console.log("Goodbye")
+// }
+
+// greet("Alice", sayGoodbye)
+
+
+
+// function multiplier(factor){
+//   // console.log(factor)
+//   return function(number){
+//     // console.log("inner function", factor)
+//     return factor * number;
+//   }
+
+// }
+// const multiplierByTwo = multiplier(2)
+// console.log(multiplierByTwo(5));
+
---
+++ test.js
@@ -0,0 +1,9 @@
+let a= 20;
+let b= 20;
+const totel =(a + b);
+console.log(totel)
+
+let c= 20;
+let d= 20;
+const inTotel =(a + b);
+console.log(inTotel)
